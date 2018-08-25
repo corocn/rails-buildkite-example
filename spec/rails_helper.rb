f97e0a261e8649b3d3d5210efc3050fdf7c1fb1a
corocn <crsk.plmzh@gmail.com>
2018-08-26 01:51:32 +0900

    add capybara inline screenshot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 require 'capybara/rspec'
-require 'capybara-screenshot/rspec'
+require 'capybara-inline-screenshot/rspec'
 require 'selenium-webdriver'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
