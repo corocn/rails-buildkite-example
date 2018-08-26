f3fe9aaaef6370149211d34457cf6c813c45e0ac
corocn <crsk.plmzh@gmail.com>
2018-08-26 17:53:47 +0900

    add rubocop

--- spec/samples/second_spec.rb
+++ spec/samples/second_spec.rb
@@ -1,7 +1,8 @@
-RSpec.describe "Secondrequire 'spec_helper'
+# frozen_string_literal: true
 
+RSpec.describe "Secondrequire 'spec_helper'
  spec" do
-  it "runs in parallel" do
+  it 'runs in parallel' do
     expect(1).to eql(1)
   end
 end
--- spec/samples/third_spec.rb
+++ spec/samples/third_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-RSpec.describe "Third spec" do
-  it "runs in parallel" do
+RSpec.describe 'Third spec' do
+  it 'runs in parallel' do
     expect(1).to eql(1)
   end
 end
